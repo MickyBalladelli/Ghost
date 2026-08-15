a1d987feb53d03a4e761de44de42094af10d406d
MickyBalladelli <micky.balladelli@gmail.com>
2026-08-15 19:39:58 +0200

    register inline completion provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,50 @@
 import * as vscode from 'vscode'
 
+import { localPilotConfig } from './config'
+import { createInlineCompletionProvider } from './providers/inlineCompletionProvider'
+
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand('localpilot-ai.helloWorld', () => {
+  const helloWorldCommand = vscode.commands.registerCommand('localpilot-ai.helloWorld', () => {
     vscode.window.showInformationMessage('LocalPilot AI is ready.')
   })
+  const inlineProvider = createInlineCompletionProvider()
+  const inlineProviderRegistration = vscode.languages.registerInlineCompletionItemProvider(
+    { pattern: '**' },
+    inlineProvider
+  )
+  const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100)
 
-  context.subscriptions.push(disposable)
+  const updateStatusBar = () => {
+    const enabled = localPilotConfig.get('enableInlineCompletions')
+    statusBar.text = enabled
+      ? '$(sparkle) LocalPilot: Inline On'
+      : '$(circle-slash) LocalPilot: Inline Off'
+    statusBar.tooltip = enabled
+      ? 'LocalPilot inline completions are enabled. Click to disable.'
+      : 'LocalPilot inline completions are disabled. Click to enable.'
+    statusBar.command = 'localpilot.toggleInline'
+    statusBar.show()
+  }
+
+  const toggleInlineCommand = vscode.commands.registerCommand('localpilot.toggleInline', async () => {
+    const enabled = localPilotConfig.get('enableInlineCompletions')
+    await localPilotConfig.update('enableInlineCompletions', !enabled)
+    updateStatusBar()
+  })
+  const configurationListener = localPilotConfig.onDidChange((settings, event) => {
+    if (event.affectsConfiguration('localpilot.enableInlineCompletions')) {
+      updateStatusBar()
+    }
+  })
+
+  updateStatusBar()
+  context.subscriptions.push(
+    helloWorldCommand,
+    inlineProviderRegistration,
+    toggleInlineCommand,
+    configurationListener,
+    statusBar
+  )
 }
 
 export function deactivate() {}
